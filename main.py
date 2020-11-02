804fd860329d3eab999b7c8ea6dd6ed7ffab3cd8
ohnchen <ohmejona@gmail.com>
2020-11-02 19:53:31 +0100

    adding comments

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import datetime
 from datetime import datetime
 from datetime import timedelta, date
 from timeit import default_timer as timer
-start = timer()
+start = timer() # start the timer for controlling how long the program takes
 
 allurl = []
-kurse = ["MA11"]
+kurse = ["Q2", "MA11", "PH11", "CH12", "de15", "la15", "po15", "rk15", "sf13", "la15-la25", "sp1323"] # for future features
 
 url1_1 = "https://emaos.de/ExportSchueler/Schueler1/subst_001.htm"
 url1_2 = "https://emaos.de/ExportSchueler/Schueler1/subst_002.htm"
@@ -20,25 +20,31 @@ url2_1 = "https://emaos.de/ExportSchueler/Schueler2/subst_001.htm"
 url2_2 = "https://emaos.de/ExportSchueler/Schueler2/subst_002.htm"
 url2_3 = "https://emaos.de/ExportSchueler/Schueler2/subst_003.htm"
 
-urls = [url1_1, url1_2, url1_3, url2_1, url2_2, url2_3]
+# Squeezing the urls inside a list to be able to iterate over them
+urls = [url1_1, url1_2, url1_3, url2_1, url2_2, url2_3] 
 
+# Get the content of all the sites and join them together
 for url in urls:
     content = requests.get(url)
     soup = BeautifulSoup(content.text, "html.parser")
     allurl.append(soup)
-
 html = "".join(str(allurl))
+
+# Preparing to get the information by the date (of today and tomorrow)
 datum = str(datetime.now().day) + "." + str(datetime.now().month) + "."  # e.g. 2.11.
 next_date = date.today() + timedelta(days=1)  
 tomorrow = str(next_date.day) + "." + str(next_date.month) + "." # e.g. 3.11
 
+# filter() is used to delete everything from the list that is a empty string; list() turns the filterobject back into a list
 liste = list(filter(None, re.findall(">(.*?)<", html)))
 
+# Get the index of every date I am searching for; enumerate() so it doesn't only take the firstitem and repeats to return that index every iteration
 index = [i for i, e in enumerate(liste) if e == datum or e == tomorrow] 
 for j in index:
     print("")
     for i in range(9):
         print(liste[j-1+i], end=" ")
 
+# Print the time the program needed to complete
 elapsed_time = timer() - start
 print(elapsed_time)
